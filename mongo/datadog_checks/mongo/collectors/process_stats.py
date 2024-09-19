bb4ecab51cd188ac37ae0db848bcc3306dbab7d7
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-09-19 13:56:12 +0000

    cache mongo process

--- mongo/datadog_checks/mongo/collectors/process_stats.py
+++ mongo/datadog_checks/mongo/collectors/process_stats.py
@@ -18,6 +18,7 @@ class ProcessStatsCollector(MongoCollector):
     def __init__(self, check, tags):
         super(ProcessStatsCollector, self).__init__(check, tags)
         self._clean_server_name = check._config.clean_server_name
+        self._process = None
 
     @property
     def is_localhost(self):
@@ -54,19 +55,22 @@ class ProcessStatsCollector(MongoCollector):
             self.log.warning("Process with PID %s not found or access denied.", pid)
             return None
 
-    def _find_pid_by_name(self, process_name):
+    def _find_process_by_name(self, process_name):
         """Find and return the PID of a process by its name."""
         if not process_name:
             self.log.warning("No process name provided.")
             return None
         for process in psutil.process_iter(["pid", "name"]):
             if process.info["name"] == process_name:
-                return process.info["pid"]
+                return process
         self.log.warning("No process found with the name %s.", process_name)
         return None
 
     def _get_mongo_process(self, api):
         """Retrieve the MongoDB process using either PID or process name."""
+        if self._process:
+            return self._process
+
         # Try to get the PID and process name from serverStatus
         pid, process_name = self._get_pid_and_process_name(api)
 
@@ -74,15 +78,14 @@ class ProcessStatsCollector(MongoCollector):
         process = self._find_process_by_pid(pid) if pid else None
 
         # If process not found by PID, attempt to find it by process name
-        if not process or process.name != process_name:
-            pid = self._find_pid_by_name(process_name)
-            if pid:
-                process = self._find_process_by_pid(pid)
+        if not process or process.name() != process_name:
+            process = self._find_process_by_name(process_name)
 
         if not process:
             self.log.warning("Unable to retrieve MongoDB process.")
 
-        return process
+        self._process = process
+        return self._process
 
     def collect(self, api):
         process = self._get_mongo_process(api)
